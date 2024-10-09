2e8a468f80a992f8bac6e8e685368bac697e4c64
Coker Alcocer <coker@AirPotato.local>
2024-10-09 14:06:13 -0600

    [UPDATE] Clase del 09/10/2024

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 const URL = 'http://localhost:8080';
 let departments = [];
+let employees = [];
+let employee = {};
 
 const findAllDepartments = async () => {
     await fetch(`${URL}/api/department`, {
@@ -10,7 +12,104 @@ const findAllDepartments = async () => {
         }
     }).then(response => response.json()).then(response => {
         console.log(response);
+        departments = response.data;
     }).catch(console.log);
 };
 
-findAllDepartments();
+const loadData = async flag => {
+    await findAllDepartments(); // Armando la lista de departmanentos
+    let select = document.getElementById(flag ? 'departments' : 'u_departments'); // Recuperando el select
+    let content = ''; // Variable de concatenación
+
+    // Concatenando opciones como contenido
+    departments.forEach(item => {
+        content += `<option value="${item.id}">${item.name}</option>`;
+    });
+    // Añadir el contenido al select
+    select.innerHTML = content;
+}
+
+const findAllEmployees = async () => {
+    await fetch(`${URL}/api/employee`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    }).then(response => response.json()).then(response => {
+        console.log(response);
+        employees = response.data;
+    }).catch(console.log);
+}
+
+const loadTable = async () => {
+    await findAllEmployees();
+    let tbody = document.getElementById('tbody');
+    let content = ''; 
+
+    employees.forEach((item, index) => {
+        content += `<tr>
+                        <th scope="row">${index + 1}</th>
+                        <td>${item.fullName}</td>
+                        <td>${item.eMail}</td>
+                        <td>${item.department.name}</td>
+                        <td class="text-center">
+                            <button class="btn btn-outline-danger">Eliminar</button>
+                            <button class="btn btn-outline-primary" data-bs-target="#updateModal" data-bs-toggle="modal" onclick="setDataOnForm(${item.id})">Editar</button>
+                        </td>
+                    </tr>`;
+    });
+    tbody.innerHTML = content;
+}
+
+// Esta función se ejecuta una sola vez al cargar el JS
+(async () => {
+    await loadTable();
+})();
+
+const save = async () => {
+    let form = document.getElementById('saveForm');
+    employee = {
+        fullName: document.getElementById('fullName').value,
+        eMail: document.getElementById('eMail').value,
+        department: {
+            id: document.getElementById('departments').value
+        }
+    };
+
+    await fetch(`${URL}/api/employee`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(employee)
+    }).then(response => response.json()).then(async response => {
+        console.log(response);
+        employee = {};
+        form.reset();
+        await loadTable();
+    }).catch(console.log);
+}
+
+const findById = async id => {
+    await fetch(`${URL}/api/employee/${id}`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    }).then(response => response.json()).then(response => {
+        console.log(response);
+        employee = response.data;
+    }).catch(console.log);
+}
+
+const setDataOnForm = async id => {
+    await findById(id);
+    loadData(false);
+
+    document.getElementById('u_fullName').value = employee.fullName;
+    document.getElementById('u_eMail').value = employee.eMail;
+    document.getElementById('u_departments').value = employee.department.id;
+}
